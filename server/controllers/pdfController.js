28ffc6d9ef260ee5aa6dac48cfc5d887b556d20a
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:38:12 +0530

    Implement PDF download for resumes
    Add functionality to download resume as PDF with user validation.

--- server/controllers/pdfController.js
+++ server/controllers/pdfController.js
@@ -1,1 +1,20 @@
+import Resume from "../models/Resume.js"
+import { generatePDF } from "../services/pdfService.js"
 
+export const downloadResumePDF = async (req, res) => {
+  const resume = await Resume.findById(req.params.id)
+
+  if (!req.user.isPro) {
+    res.status(403)
+    throw new Error("Upgrade to PRO")
+  }
+
+  const pdf = await generatePDF(resume)
+
+  res.set({
+    "Content-Type": "application/pdf",
+    "Content-Disposition": "attachment; filename=resume.pdf"
+  })
+
+  res.send(pdf)
+}
