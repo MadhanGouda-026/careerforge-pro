fd4782c48da5453b73a39fbbee2c3bc1e4a22a80
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:32:25 +0530

    Add error handling middleware for 404 and general errors

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,1 +1,10 @@
+export const notFound = (req, res) => {
+  res.status(404)
+  throw new Error("Route Not Found")
+}
 
+export const errorHandler = (err, req, res, next) => {
+  res.status(res.statusCode || 500).json({
+    message: err.message
+  })
+}
