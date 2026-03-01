573e6988d3080f13644b10371ebd0a1eeffe4d6c
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:31:13 +0530

    Add User model with password hashing and comparison

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    isPro: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+export default mongoose.model("User", userSchema)
