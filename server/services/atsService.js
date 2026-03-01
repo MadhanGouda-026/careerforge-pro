b18fa0e23f553cb5da1e827f2a22bf06c51b8141
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:33:13 +0530

    Add calculateATSScore function to atsService

--- server/services/atsService.js
+++ server/services/atsService.js
@@ -1,1 +1,7 @@
-
+export const calculateATSScore = (resumeText, keywords) => {
+  let match = 0
+  keywords.forEach((k) => {
+    if (resumeText.includes(k)) match++
+  })
+  return Math.round((match / keywords.length) * 100)
+}
