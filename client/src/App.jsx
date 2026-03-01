b7d6eadfb443c9a622c6895c7302e65987d00b70
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:52:07 +0530

    Set up routing with authentication in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+import ProtectedRoute from "./components/ProtectedRoute"
 
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+import ResumeBuilder from "./pages/ResumeBuilder"
+import Pricing from "./pages/Pricing"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/dashboard"
+            element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
+          <Route path="/builder"
+            element={<ProtectedRoute><ResumeBuilder /></ProtectedRoute>} />
+
+          <Route path="/pricing"
+            element={<ProtectedRoute><Pricing /></ProtectedRoute>} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
