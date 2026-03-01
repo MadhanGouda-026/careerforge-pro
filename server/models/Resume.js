bf4b8137c008b86a6468fdef231257400794b8d2
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:31:37 +0530

    Add Resume model with schema definition

--- server/models/Resume.js
+++ server/models/Resume.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose"
 
+const resumeSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    title: String,
+    content: Object,
+    atsScore: Number
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model("Resume", resumeSchema)
