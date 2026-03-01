87b7c7517b7a84dfa9428ba0e7dbd80447343e3f
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 12:23:17 +0530

    Update webhook route to use express.raw middleware

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -5,6 +5,6 @@ import { checkout, stripeWebhook } from "../controllers/paymentController.js"
 const router = express.Router()
 
 router.post("/checkout", protect, checkout)
-router.post("/webhook", stripeWebhook)
+router.post("/webhook",express.raw({ type: "application/json" }), stripeWebhook)
 
 export default router
