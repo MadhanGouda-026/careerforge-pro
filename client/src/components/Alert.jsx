3a85d5aa2103c46388c0fc7af80bd71c4a08e318
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 16:38:54 +0530

    Update Alert.jsx

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,1 +1,15 @@
+const Alert = ({ message, type = "success" }) => {
+  if (!message) return null;
 
+  return (
+    <div
+      className={`p-3 rounded-md text-white ${
+        type === "error" ? "bg-red-500" : "bg-green-500"
+      }`}
+    >
+      {message}
+    </div>
+  );
+};
+
+export default Alert;
