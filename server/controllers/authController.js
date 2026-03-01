879bcc38b54630c52dcb7eaab80684b68f077a87
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:35:37 +0530

    Add user registration and login with JWT
    Implement user registration and login functionality with JWT token generation.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,23 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
 
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" })
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body
+  const user = await User.create({ name, email, password })
+  res.json({ ...user._doc, token: generateToken(user._id) })
+}
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({ ...user._doc, token: generateToken(user._id) })
+  } else {
+    res.status(401)
+    throw new Error("Invalid credentials")
+  }
+}
