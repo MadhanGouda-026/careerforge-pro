0eb49e45eff44105f8c1a11ae93ab297a8ef519a
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:38:54 +0530

    Add generateCoverLetter function to create cover letters

--- server/controllers/coverLetterController.js
+++ server/controllers/coverLetterController.js
@@ -1,1 +1,15 @@
+export const generateCoverLetter = async (req, res) => {
+  const { name, role, company } = req.body
 
+  const letter = `
+Dear Hiring Manager,
+
+My name is ${name}. I am excited to apply for the ${role} position at ${company}.
+I believe my skills and experience align perfectly.
+
+Sincerely,
+${name}
+`
+
+  res.json({ letter })
+}
