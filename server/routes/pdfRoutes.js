dc08109a814215ef576f147c71186134c51e3dca
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:40:33 +0530

    Add PDF download route with authentication

--- server/routes/pdfRoutes.js
+++ server/routes/pdfRoutes.js
@@ -1,1 +1,9 @@
+import express from "express"
+import { protect } from "../middleware/authMiddleware.js"
+import { downloadResumePDF } from "../controllers/pdfController.js"
 
+const router = express.Router()
+
+router.get("/:id/download", protect, downloadResumePDF)
+
+export default router
