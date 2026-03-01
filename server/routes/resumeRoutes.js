4e4a10690148ebcaeaac297ff010981d7eb883af
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:39:49 +0530

    Add resume routes with authentication protection

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,1 +1,17 @@
+import express from "express"
+import { protect } from "../middleware/authMiddleware.js"
+import {
+  createResume,
+  getResumes,
+  deleteResume
+} from "../controllers/resumeController.js"
 
+const router = express.Router()
+
+router.route("/")
+  .post(protect, createResume)
+  .get(protect, getResumes)
+
+router.delete("/:id", protect, deleteResume)
+
+export default router
