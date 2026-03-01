11b9028191815d7e6445bc531832b1e626edcc62
Madhan Madhav Gouda <goudamadhan226@gmail.com>
2026-03-01 11:32:50 +0530

    Add keyword extraction functionality

--- server/services/keywordService.js
+++ server/services/keywordService.js
@@ -1,1 +1,16 @@
+import natural from "natural"
 
+const tokenizer = new natural.WordTokenizer()
+
+export const extractKeywords = (text) => {
+  const words = tokenizer.tokenize(text.toLowerCase())
+  const frequency = {}
+
+  words.forEach((w) => {
+    if (w.length > 3) frequency[w] = (frequency[w] || 0) + 1
+  })
+
+  return Object.keys(frequency)
+    .sort((a, b) => frequency[b] - frequency[a])
+    .slice(0, 20)
+}
